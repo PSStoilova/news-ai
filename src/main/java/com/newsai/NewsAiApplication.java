83828cec4b8082f43a92685cd9e02d386091f373
PSStoilova <petya.ssp@gmail.com>
2025-08-17 17:29:07 +0200

    initial commit

---
+++ src/main/java/com/newsai/NewsAiApplication.java
@@ -0,0 +1,13 @@
+package com.newsai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newsai/NewsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
